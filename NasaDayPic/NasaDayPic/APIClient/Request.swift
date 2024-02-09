701a59bbaa34e6c16bf2bae494d85f380a3ba302
Annitas <39757257+Annitas@users.noreply.github.com>
2024-02-09 16:23:36 +0600

    Add api related files

---
+++ NasaDayPic/NasaDayPic/APIClient/Request.swift
@@ -0,0 +1,12 @@
+//
+//  Request.swift
+//  NasaDayPic
+//
+//  Created by Anita Stashevskaya on 09.02.2024.
+//
+
+import Foundation
+
+final class Request {
+    
+}
---
+++ NasaDayPic/NasaDayPic/APIClient/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  NasaDayPic
+//
+//  Created by Anita Stashevskaya on 09.02.2024.
+//
+
+import Foundation
+
+final class Service {
+    static let shared = Service()
+    
+    private init() {}
+    
+    public func execute(_ request: Request, completion: @escaping () -> Void) {
+        
+    }
+}
